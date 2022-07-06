df962d28d3333a422799464150a1e109b0e94c2c
Imxavi31081999 <imravishankar00@gmail.com>
2022-07-06 12:56:51 +0530

    new keyframe section after team

--- js/readmoreless.js
+++ js/readmoreless.js
@@ -34,3 +34,29 @@ $(document).ready(function() {
         return false;
     });
 });
+
+
+/* 2. Cases */
+document.addEventListener('DOMContentLoaded', () => {
+    if (document.querySelector('.js-cases-row')) {
+        const row = document.querySelector('.js-cases-row');
+        const casesCard = [...row.querySelectorAll('.js-cases-card')];
+
+        function cloneNodes() {
+            casesCard.forEach((card) => {
+                const clone = card.cloneNode(true)
+
+                clone.dataset.clone = true
+
+                row.appendChild(clone)
+            })
+        }
+        cloneNodes()
+
+        casesCard.forEach((card) => {
+            row.appendChild(card)
+        })
+
+        cloneNodes()
+    }
+});
